d8313158ba592f996034deea51410e6dd115f2c4
Mosarof Hossen <mosarofhossen5228@mail.com>
2022-09-05 11:00:47 +0600

    filter()

---
+++ filter.js
@@ -0,0 +1,15 @@
+//filter() almost map er motoi.
+//ati array  er  sathe bose tar protite element k nibe
+//and parent thesis er bitore kicu kaj kore arekti array return kore.
+//nicher filter() parent thesis er bitore akti arrow function nia bole disi
+//j number  golu 15 er theke boro segoke return korbe.
+
+const numbers = [20, 70, 50, 90, 80, 53, 83, 30, 60, 100, 12, 13, 15, 18];
+const bigNumbers = numbers.filter(x => x > 15);
+const tinyNumbers = numbers.filter(x => x < 20);
+const eventNumber = numbers.filter(num => num % 2 == 0);
+console.log(bigNumbers);
+console.log(tinyNumbers)
+console.log(eventNumber);
+
+
